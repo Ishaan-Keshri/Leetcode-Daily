45957f701c9d3ec3205d4afb4212533102bb0010
Ishaan-Keshri <ishaankeshri10@gmail.com>
2026-08-29 14:50:21 +0530

    Time: 531 ms (48.09%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,6 @@
-/* Write your PL/SQL query statement below */
-select w1.id
-from Weather w1 
-join Weather w2
-on (w1.recordDate-w2.recordDate)=1
-and w1.temperature>w2.temperature;
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
