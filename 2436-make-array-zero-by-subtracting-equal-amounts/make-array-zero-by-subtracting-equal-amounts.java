a393d3456921a36d6bb8d893d9bb2d5a7c21c3d9
Ishaan-Keshri <ishaankeshri10@gmail.com>
2026-09-07 18:48:37 +0530

    Time: 1 ms (92.74%) | Memory: 42.7 MB (96.29%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int n:nums)
+        {
+            if(n!=0)
+            {
+                set.add(n);
+            }
+        }
+        return set.size();
+    }
+}
