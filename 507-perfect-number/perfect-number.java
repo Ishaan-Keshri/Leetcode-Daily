a64484089913eb2d69a1a547a107af13b526a91e
Ishaan-Keshri <ishaankeshri10@gmail.com>
2026-09-18 23:59:13 +0530

    Time: 2078 ms (61.77%) | Memory: 42.3 MB (25.63%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int s=0;
+        for(int i=1;i<=(int)num/2;i++)
+        {
+            if(num%i==0)
+            {
+                s=s+i;
+            }
+        }
+        if(s==num)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
